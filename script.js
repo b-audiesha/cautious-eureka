6858d6aa739da306114310c8d0d3169d71794ff4
audiesha brattin <b-audiesha@gmail.com>
2020-12-22 21:19:49 -0600

    adding udemy work

---
+++ script.js
@@ -0,0 +1,4 @@
+/*alert('hello');
+document.write('Audiesha Brattin');*/ /*will appear wherever the script tag is but not an idea way to print to document*/
+document.getElementById('message').innerHTML ='Audi Brattin'; /*Changed h1 text to "audi brattin"*/
+
