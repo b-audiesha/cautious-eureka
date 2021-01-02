c64f98ab1667c71217bb31a8b0dfcb84dd3c3aad
audiesha brattin <a.brattin@gmail.com>
2021-01-02 14:56:46 -0600

    variable practice

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 /*alert('hello');
 document.write('Audiesha Brattin');*/ /*will appear wherever the script tag is but not an idea way to print to document*/
-document.getElementById('message').innerHTML ='Audi Brattin'; /*Changed h1 text to "audi brattin"*/
+/*
+document.getElementById('message').innerHTML ='Audi Brattin'; /!*Changed h1 text to "audi brattin"*!/
+
+*/
+/*const myName = 'Audiesha Brattin' const cannot be changed let allows your to re-assign value to a variable.*/
+let myName = 'Audiesha Brattin';
+myName = 'Audi Brattin';
+
+if(true){
+  let myName = 'Tester';
+  console.log(myName);
+}
+
+
+
+
+alert(myName);
+document.write(myName);
+document.getElementById('message').innerHTML= myName;
+console.log(myName);
+/*
+Variable Names can contain letters,digits,underscores and dollar signs. First charagter must begin witha letter or $.*/
 
