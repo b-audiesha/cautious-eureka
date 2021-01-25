18b7ad4a1028d9328339ad517bc259c0444e1ba5
audiesha brattin <a.brattin@gmail.com>
2021-01-24 19:50:36 -0600

    coding challenge

---
+++ coding_challenges.js
@@ -0,0 +1,6 @@
+let person = prompt('Hey ! What is your name?');
+let message = "Thanks for visiting my website, " + person;
+console.log(message);
+document.write(message);
+document.getElementById('headline').innerHTML = message;
+
--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-/*alert('hello');
-document.write('Audiesha Brattin');*/ /*will appear wherever the script tag is but not an idea way to print to document*/
-/*
-document.getElementById('message').innerHTML ='Audi Brattin'; /!*Changed h1 text to "audi brattin"*!/
+let myName = prompt("What is your name?");
+let message = "Welcome to the page, ";
+let output = message + myName;
 
-*/
-/*const myName = 'Audiesha Brattin' const cannot be changed let allows your to re-assign value to a variable.*/
-let myName = 'Audiesha Brattin';
-myName = 'Audi Brattin';
-
-if(true){
-  let myName = 'Tester';
-  console.log(myName);
-}
-
-
-
-
-alert(myName);
+console.log(output);
 document.write(myName);
 document.getElementById('message').innerHTML= myName;
 console.log(myName);
 /*
+
+
+
 Variable Names can contain letters,digits,underscores and dollar signs. First charagter must begin witha letter or $.*/
 
